cf22cc2a2fdb7224a03495b25de19f33997f319a
yacoub abakar <yacoubmahamat2015@gmail.com>
2021-03-01 22:06:37 -0500

    updated projects

--- src/components/data/projects_data.js
+++ src/components/data/projects_data.js
@@ -1,10 +1,33 @@
+
+import gif from '../../assets/gif.png'
 const projects_data = [
     {
+        name: 'Gif',
+        image: '../../assets/gif.png',
+        deployed_url: 'https://github.com/yacoub05/gify_project',
+        github_url: 'https://github.com/yacoub05/gify_project',
+        category: ['react.js']
+    },
+    {
         name: '',
-        image: "",
-        deployed_url: '',
-        github_url: '',
-        category: []
+        image: '../../assets/gif.png',
+        deployed_url: 'https://github.com/yacoub05/zip-search',
+        github_url: 'https://github.com/yacoub05/zip-search',
+        category: ['react.js']
+    },
+    {
+        name: '',
+        image: '../../assets/gif.png',
+        deployed_url: 'https://github.com/yacoub05/adultblocker-chrome-extension',
+        github_url: 'https://github.com/yacoub05/adultblocker-chrome-extension',
+        category: ['react.js']
+    },
+    {
+        name: '',
+        image: '../../assets/gif.png',
+        deployed_url: 'https://github.com/yacoub05/iqraproject',
+        github_url: 'https://github.com/yacoub05/iqraproject',
+        category: ['react.js']
     },
 
     
